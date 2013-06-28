47aef021c320666e2a7f926827e87f160710c3bb
Yasuharu NAKANO <ynak@jggug.org>
2013-06-28 10:43:51 +0900

    added BlackdragSpec to think about his sample

---
+++ src/test/groovy/org/jggug/kobo/dci/BlackdragSpec.groovy
@@ -0,0 +1,99 @@
+package org.jggug.kobo.dci
+
+import spock.lang.Specification
+
+import java.util.concurrent.ArrayBlockingQueue
+import java.util.concurrent.CountDownLatch
+
+class BlackdragSpec extends Specification {
+
+    Person person
+
+    def setupSpec() {
+        ObjectExtension.extendMetaClass()
+    }
+
+    def setup() {
+        person = new Person(name: "Obama")
+    }
+
+    def "nobeans: my sample"() {
+        expect:
+        person.withMixin(PresidentRole) {
+            person.sayHelloTo("Mike") == "Hello, Mike."
+        }
+
+        and:
+        person.withMixin(FatherRole) {
+            person.sayHelloTo("Michelle") == "Hello, my honey?"
+        }
+
+        when:
+        person.sayHelloTo("Ooops")
+
+        then:
+        thrown MissingMethodException
+    }
+
+    def "blackdrag: (A) Multithread Context"() {
+        given:
+        def history = new ArrayBlockingQueue(10)
+        def startLatch = new CountDownLatch(2)
+        def endLatch = new CountDownLatch(2)
+        def waitForStarting = { id ->
+            history << "${id}:waiting start"
+            startLatch.countDown()
+            history << "${id}:started"
+        }
+        def waitForEnding = { id ->
+            history << "${id}:waiting end"
+            endLatch.countDown()
+            history << "${id}:ended"
+        }
+
+        when: "T1 doing this"
+        Thread.start {
+            person.withMixin(PresidentRole) {
+                waitForStarting("T1")
+                try {
+                    assert person.sayHelloTo("Mike") == "Hello, Mike."
+                } finally {
+                    waitForEnding("T1")
+                }
+            }
+        }
+
+        and: "and T2 doing this"
+        Thread.start {
+            person.withMixin(FatherRole) {
+                waitForStarting("T2")
+                try {
+                    assert person.sayHelloTo("Michelle") == "Hello, my honey?"
+                } finally {
+                    waitForEnding("T2")
+                }
+            }
+        }
+
+        then:
+        history == []
+    }
+
+    static class Person {
+        String name
+    }
+
+    @Category(Person)
+    static class FatherRole {
+        String sayHelloTo(String name) {
+            "Hello, my honey?"
+        }
+    }
+
+    @Category(Person)
+    static class PresidentRole {
+        String sayHelloTo(String name) {
+            "Hello, ${name}."
+        }
+    }
+}
